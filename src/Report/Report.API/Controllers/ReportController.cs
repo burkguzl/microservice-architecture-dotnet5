516fee913624c6692f398a0d48526b767bfcbe83
burakguzel <burak.guzel@solvepark.com>
2021-01-03 13:55:20 +0300

    Create ReportAPI and add class libraries to necessary to the report microservice project

---
+++ src/Report/Report.API/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Report.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+
+    }
+}
