e82dc3bf5f1df8070738ac56786228a9681993bb
burakguzel <burak.guzel@solvepark.com>
2021-01-03 12:00:18 +0300

    Create EventBusRabbitMQProducer class and perform the necessary method.

---
+++ src/Common/EventBusRabbitMQ/Constants/EventBusConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Constants
+{
+    public static class EventBusConstants
+    {
+        public static string PrepareReportQueue = "prepareReportQueue";
+    }
+}
---
+++ src/Common/EventBusRabbitMQ/Events/PrepareReportEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Events
+{
+    public class PrepareReportEvent
+    {
+        public Guid RequestId { get; set; }
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public ICollection<Address> Addresses { get; set; }
+    }
+    public class Address
+    {
+        public string Id { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string AddressType { get; set; }
+    }
+}
